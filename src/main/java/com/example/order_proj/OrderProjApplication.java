af8c44b8459c137ee24cd2fddebefd14f052d933
junghyunPark <wjdguswjdal@naver.com>
2025-04-02 14:15:10 +0900

    커밋 테스트

--- src/main/java/com/example/order_proj/OrderProjApplication.java
+++ src/main/java/com/example/order_proj/OrderProjApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrderProjApplication {
 
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(OrderProjApplication.class, args);
 	}
