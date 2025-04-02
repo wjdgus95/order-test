0a5ba73da7726c4a3bd14e3f471d4e9ea2c06340
junghyunPark <wjdguswjdal@naver.com>
2025-04-02 09:08:57 +0900

    spring boot 초기 세팅

---
+++ src/main/java/com/example/order_proj/OrderProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.order_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProjApplication.class, args);
+	}
+
+}
